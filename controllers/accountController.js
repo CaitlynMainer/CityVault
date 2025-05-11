b9978fce3963bd13cc19db62e19718dce9ef7d26
Michiyo <michiyo@users.noreply.github.com>
2025-05-10 22:47:45 -0700

    Move everything to new SQLConfig

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,15 +1,15 @@
 const sql = require('mssql');
-const dbConfig = require('../dbConfig');
+const { getAuthPool } = require(global.BASE_DIR + '/db');
 const { gameHashPassword } = require('../utils/hashUtils');
 
 async function showAccountPage(req, res) {
   //if (!req.session.username) return res.redirect('/login');
 
   try {
-    const pool = await sql.connect(dbConfig);
+    const pool = await getAuthPool();
     const result = await pool.request()
       .input('account', sql.VarChar, req.session.username)
-      .query('SELECT email FROM cohauth.dbo.user_account WHERE account = @account');
+      .query('SELECT email FROM dbo.user_account WHERE account = @account');
 
     const email = result.recordset[0]?.email || '';
 
@@ -29,40 +29,38 @@ async function updateAccount(req, res) {
   if (!req.session.username) return res.redirect('/login');
 
   const { email, oldPassword, newPassword } = req.body;
+  const account = req.session.username;
 
   try {
-    const pool = await sql.connect(dbConfig);
-    const account = req.session.username;
+    const pool = await getAuthPool();
 
     const result = await pool.request()
       .input('account', sql.VarChar, account)
-      .query('SELECT password FROM cohauth.dbo.user_auth WHERE account = @account');
+      .query('SELECT password FROM dbo.user_auth WHERE account = @account');
 
-      const storedHash = Buffer.from(result.recordset[0].password).toString('utf8').toLowerCase();
-      const inputHash = gameHashPassword(account, oldPassword).toString('hex').toLowerCase();
-      
-      if (!storedHash.startsWith(inputHash)) {
-        return res.send(`<p style="color:red">❌ Incorrect current password.</p>`);
-      }
+    const storedHash = Buffer.from(result.recordset[0].password).toString('utf8').toLowerCase();
+    const inputHash = gameHashPassword(account, oldPassword).toString('hex').toLowerCase();
+
+    if (!storedHash.startsWith(inputHash)) {
+      return res.send(`<p style="color:red">❌ Incorrect current password.</p>`);
+    }
 
     if (email) {
       await pool.request()
         .input('account', sql.VarChar, account)
         .input('email', sql.VarChar, email)
-        .query('UPDATE cohauth.dbo.user_account SET email = @email WHERE account = @account');
+        .query('UPDATE dbo.user_account SET email = @email WHERE account = @account');
     }
 
     if (newPassword) {
-        const request = new sql.Request();
-    
-        const hashBuffer = gameHashPassword(account, newPassword); // returns a 64-byte Buffer
-        const hexString = hashBuffer.toString('hex'); // convert to 128-character hex string for MSSQL
-    
-        request.input('account', sql.VarChar, account);
-        request.input('password', sql.VarChar, hexString); // use VarChar for CONVERT(BINARY(128), ...)
-    
-        await request.query(`
-          UPDATE cohauth.dbo.user_auth
+      const hashBuffer = gameHashPassword(account, newPassword);
+      const hexString = hashBuffer.toString('hex');
+
+      await pool.request()
+        .input('account', sql.VarChar, account)
+        .input('password', sql.VarChar, hexString)
+        .query(`
+          UPDATE dbo.user_auth
           SET password = CONVERT(BINARY(128), @password)
           WHERE account = @account
         `);
